844b5c6e344920141d0ecc24175037f9065c876c
Huiting Tan <huitingt@L1CFCPVP9X.local>
2025-01-17 09:47:50 +1100

    Add djangoframework to requirements

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
 
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
